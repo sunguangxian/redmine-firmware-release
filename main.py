11d740eba527d49bf4a43ebf5049ea1b13988d88
sunguangxian <38075908+sunguangxian@users.noreply.github.com>
2026-07-04 22:13:38 +0800

    Register mail test API

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from release_tool.api_app import main
+from release_tool.api_app import app, main
+from release_tool.mail_test_api import register_mail_test_routes
+
+register_mail_test_routes(app)
 
 if __name__ == "__main__":
     main()
