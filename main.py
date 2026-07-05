62add4050a596646cd9ddad4242d04c98429c157
sunguangxian <38075908+sunguangxian@users.noreply.github.com>
2026-07-05 09:01:29 +0800

    注册邮件闭环增强接口

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from release_tool.api_app import app, main
 from release_tool.health_api import register_health_routes
 from release_tool.index_sync_patch import apply_index_sync_patches
+from release_tool.mail_admin_test_api import register_admin_mail_test_routes
+from release_tool.mail_history_patch import apply_mail_history_patch
+from release_tool.mail_log_api import register_mail_log_routes
 from release_tool.mail_test_api import register_mail_test_routes
 from release_tool.release_ops_api import register_release_ops_routes
 from release_tool.secure_config import apply_secure_config_patches
@@ -22,8 +25,11 @@ def move_frontend_routes_to_end():
 
 apply_secure_config_patches()
 apply_index_sync_patches()
+apply_mail_history_patch()
 register_session_guard(app)
 register_mail_test_routes(app)
+register_admin_mail_test_routes(app)
+register_mail_log_routes(app)
 register_health_routes(app)
 register_release_ops_routes(app)
 move_frontend_routes_to_end()
