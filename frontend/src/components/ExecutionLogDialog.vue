5e2863d41715395b83543f7c9bb8d0fd976a98a1
sunguangxian <38075908+sunguangxian@users.noreply.github.com>
2026-07-05 17:22:46 +0800

    新增执行日志弹窗组件

---
+++ frontend/src/components/ExecutionLogDialog.vue
@@ -0,0 +1,19 @@
+<template>
+  <el-dialog v-model="visible" title="执行日志" width="820px" destroy-on-close>
+    <div class="release-log">
+      <div v-for="(item, index) in logs" :key="index">{{ index + 1 }}. {{ item }}</div>
+    </div>
+  </el-dialog>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+
+const props = defineProps<{ modelValue: boolean; logs: string[] }>()
+const emit = defineEmits<{ (event: 'update:modelValue', value: boolean): void }>()
+
+const visible = computed({
+  get: () => props.modelValue,
+  set: (value: boolean) => emit('update:modelValue', value),
+})
+</script>
