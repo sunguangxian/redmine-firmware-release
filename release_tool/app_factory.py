dc39656678b7082fde390a960cc794b7e55d31fc
sunguangxian <38075908+sunguangxian@users.noreply.github.com>
2026-07-05 10:17:48 +0800

    启用内联发布模式补丁

--- release_tool/app_factory.py
+++ release_tool/app_factory.py
@@ -8,12 +8,14 @@ from .api_app import app as app
 from .auth_api import register_auth_routes
 from .health_api import register_health_routes
 from .index_sync_patch import apply_index_sync_patches
+from .inline_release_patch import apply_inline_release_patches
 from .mail_admin_test_api import register_admin_mail_test_routes
 from .mail_history_patch import apply_mail_history_patch
 from .mail_log_api import register_mail_log_routes
 from .mail_policy import apply_mail_policy_patches
 from .mail_settings_api import register_mail_settings_routes
 from .mail_test_api import register_mail_test_routes
+from .release_catalog_api import register_release_catalog_routes
 from .release_ops_api import register_release_ops_routes
 from .release_publish_api import register_release_publish_routes
 from .secure_config import apply_secure_config_patches
@@ -42,6 +44,7 @@ def create_app():
     apply_secure_config_patches()
     apply_mail_policy_patches()
     apply_index_sync_patches()
+    apply_inline_release_patches()
     apply_mail_history_patch()
     register_auth_routes(app)
     register_session_guard(app)
@@ -50,6 +53,7 @@ def create_app():
     register_mail_settings_routes(app)
     register_mail_log_routes(app)
     register_health_routes(app)
+    register_release_catalog_routes(app)
     register_release_ops_routes(app)
     register_release_publish_routes(app)
     register_wiki_config_routes(app)
